50c0e2c292a5eae18bb60a84cce19fa08244ca81
Fei Ge <feitechnique@gmail.com>
2019-11-18 17:58:35 +0100

    undone

---
+++ lescodes/279-perfectsquares.cxx
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> nums;
+        int nums = 0;
+        for(int i=n;i<=0;i++){
+            
+        }
+        
+    }
+};
